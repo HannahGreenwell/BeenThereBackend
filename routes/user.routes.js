4f1267979ade67b953ba5c9e9ea1497c53187c73
Hannah Greenwell <hannah@Hannahs-MacBook-Pro.local>
2018-11-26 17:23:42 +1100

    Post to signup working (with Postman) and refactored server.js into route file

---
+++ routes/user.routes.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const passportJWT = require('passport-jwt');
+
+///// Passport JWT Strategy
+const ExtractJwt = passportJWT.ExtractJwt;
+const JwtStrategy = passportJWT.Strategy;
+
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  console.log('payload received', jwt_payload);
+  const user = users[_.findIndex(users, {id: jwt_payload.id})];
+  if(user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
+
+///// POST TO SIGNUP
+router.post('/signup', (req, res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if(err) {
+      return res.status(500).json({
+        error: err
+      });
+    } else {
+      req.db.collection('users').insert({email: req.body.email, password: hash}, (err, result) => {
+        if(err) {
+          return res.status(500).json({error: err});
+        } else {
+          console.log(result);
+          res.status(200).json({succes: 'New user has been created'});
+        }
+      });
+    }
+  })
+});
+
+///// POST TO LOGIN
+router.post("/login", (req, res, next) => {
+  console.log(req.body);
+
+  let email;
+  let password;
+
+  if(req.body.email && req.body.password) {
+    email = req.body.email;
+    password = req.body.password;
+  }
+
+  req.db.collection('users').findOne({email}, (err, result) => {
+    const user = result;
+
+    if(!user) {
+       res.status(401).json({message: 'no such user found'});
+       return next();
+    }
+
+    if(user.password === req.body.password) {
+      const payload = {id: user.id};
+      const token = jwt.sign(payload, jwtOptions.secretOrKey);
+      res.json({message: "ok", token: token});
+    } else {
+      res.status(401).json({message: 'passwords did not match'});
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,114 +1,95 @@
-const _ = require('lodash');
 const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-
-const passport = require('passport');
-const passportJWT = require('passport-jwt');
-
-const ExtractJwt = passportJWT.ExtractJwt;
-const JwtStrategy = passportJWT.Strategy;
-
-const users = [
-  {
-    id: 1,
-    name: 'Hannah',
-    password: '%2yx4'
-  },
-  {
-    id: 2,
-    name: 'Jesica',
-    password: 'chicken'
-  }
-];
-
-///// DATABASE
-const MongoClient = require('mongodb').MongoClient;
-let db;
-
-MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
-  if(err) return console.warn(err);
-
-  db = client.db('been-there-test');
-});
-
-///// Passport JWT Strategy
-let jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = 'tasmanianDevil';
-
-const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
-  console.log('payload received', jwt_payload);
-  const user = users[_.findIndex(users, {id: jwt_payload.id})];
-  if(user) {
-    next(null, user);
-  } else {
-    next(null, false);
-  }
-});
-
-passport.use(strategy);
-
 const app = express();
+const cors = require('cors');
+const passport = require('passport');
+
+
+///// Allows route files to access db variable through req.db
+const mongoExpress = require('express-mongo-db');
+app.use(mongoExpress('mongodb://127.0.0.1:27017/been-there-test'));
+
+const user = require('./routes/user.routes');
+
+// ///// Passport JWT Strategy
+// const ExtractJwt = passportJWT.ExtractJwt;
+// const JwtStrategy = passportJWT.Strategy;
+//
+// let jwtOptions = {};
+// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// jwtOptions.secretOrKey = 'tasmanianDevil';
+//
+// const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+//   console.log('payload received', jwt_payload);
+//   const user = users[_.findIndex(users, {id: jwt_payload.id})];
+//   if(user) {
+//     next(null, user);
+//   } else {
+//     next(null, false);
+//   }
+// });
+//
+// passport.use(strategy);
+
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
+app.use('/user', user);
 
 
 ///// POST TO LOGIN
-app.post("/login", (req, res, next) => {
-  console.log(req.body);
-
-  let email;
-  let password;
-
-  if(req.body.email && req.body.password) {
-    email = req.body.email;
-    password = req.body.password;
-  }
-
-  // Replace with DB call
-  // const user = users[_.findIndex(users, {name: name})];
-
-  db.collection('users').findOne({email}, (err, result) => {
-    // if(err) return console.warn(err);
-
-    const user = result;
-
-    if(!user) {
-       res.status(401).json({message: 'no such user found'});
-       return next();
-    }
-
-    // console.log(user.password, req.body.password);
-    if(user.password === req.body.password) {
-      const payload = {id: user.id};
-      const token = jwt.sign(payload, jwtOptions.secretOrKey);
-      res.json({message: "ok", token: token});
-    } else {
-      res.status(401).json({message: 'passwords did not match'});
-    }
-  });
-
-  // if(!user) {
-  //   res.status(401).json({message: 'no such user found'});
-  // }
-  //
-  // if(user.password === req.body.password) {
-  //   // from now on we'll identify the user by the id and the id
-  //   // is the only personalised value that goes into the token
-  //   const payload = {id: user.id};
-  //   const token = jwt.sign(payload, jwtOptions.secretOrKey);
-  //   res.json({message: "ok", token: token});
-  // } else {
-  //   res.status(401).json({message: 'passwords did not match'});
-  // }
-});
+// app.post("/login", (req, res, next) => {
+//   console.log(req.body);
+//
+//   let email;
+//   let password;
+//
+//   if(req.body.email && req.body.password) {
+//     email = req.body.email;
+//     password = req.body.password;
+//   }
+//
+//   db.collection('users').findOne({email}, (err, result) => {
+//     const user = result;
+//
+//     if(!user) {
+//        res.status(401).json({message: 'no such user found'});
+//        return next();
+//     }
+//
+//     if(user.password === req.body.password) {
+//       const payload = {id: user.id};
+//       const token = jwt.sign(payload, jwtOptions.secretOrKey);
+//       res.json({message: "ok", token: token});
+//     } else {
+//       res.status(401).json({message: 'passwords did not match'});
+//     }
+//   });
+// });
 
 app.get("/secret", passport.authenticate('jwt', {session: false}), function(req, res) {
   res.json("Success! You can not see this without a token");
 });
+
+//
+// app.post('/signup', (req, res) => {
+//   bcrypt.hash(req.body.password, 10, (err, hash) => {
+//     if(err) {
+//       return res.status(500).json({
+//         error: err
+//       });
+//     } else {
+//       db.collection('users').insertOne({email: req.body.email, password: hash}, (err, result) => {
+//         if(err) {
+//           return res.status(500).json({error: err});
+//         } else {
+//           console.log(result);
+//           res.status(200).json({succes: 'New user has been created'});
+//         }
+//       });
+//     }
+//   })
+// });
 
 
 // ///// SHOW USER'S BEEN THERE MAP
