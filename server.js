d6341c7e13b6e132c9b21523c5b1703af91259a0
Hannah Greenwell <hannah@Hannahs-MacBook-Pro.local>
2018-11-28 18:10:13 +1100

    use mLab URL

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 
 ///// Allows route files to access db variable through req.db
 const mongoExpress = require('express-mongo-db');
-app.use(mongoExpress('mongodb://127.0.0.1:27017/been-there-test'));
+// app.use(mongoExpress('mongodb://127.0.0.1:27017/been-there-test'));
 
 app.use(mongoExpress(`mongodb://${process.env.MLAB_DB_USER}:${process.env.MLAB_DB_PASSWORD}@ds221990.mlab.com:21990/been-there-test`));
 
